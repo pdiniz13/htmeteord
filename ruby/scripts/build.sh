4a9c8511c8c0888c1588ca48b60bce6d98f3171f
Paulo Diniz <pdiniz_100@yahoo.com>
2016-08-04 12:52:42 -0700

    replaced wkhtmltopdf with bin version

--- ruby/scripts/build.sh
+++ ruby/scripts/build.sh
@@ -10,12 +10,18 @@ echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /u
 echo "deb http://ftp.us.debian.org/debian/ oldstable main contrib non-free" >> /etc/apt/sources.list
 echo "deb-src http://ftp.us.debian.org/debian/ oldstable main contrib non-free" >> /etc/apt/sources.list
 apt-get -y update
-apt-get install -y nodejs mysql-client postgresql-client wkhtmltopdf git libsqlite3-dev nano zlib1g redis-server rabbitmq-server graphviz ghostscript libcdt5 libglapi-mesa libgvc6 librsvg2-common libtiffxx5 libxdot4 libgvpr2 imagemagick libmagickcore5-extra libmagickcore-dev libmagickwand-dev ghostscript gsfonts-x11 netpbm libpaper-utils libpathplan4 ntp oracle-java8-installer oracle-java8-set-default unzip xfonts-base xorg-sgml-doctools discover htop libcroco3 liblzo2-2 libudev1 libpthread-stubs0-dev --no-install-recommends
+apt-get install -y nodejs mysql-client postgresql-client wkhtmltopdf xvfb git libsqlite3-dev nano zlib1g redis-server rabbitmq-server graphviz ghostscript libcdt5 libglapi-mesa libgvc6 librsvg2-common libtiffxx5 libxdot4 libgvpr2 imagemagick libmagickcore5-extra libmagickcore-dev libmagickwand-dev ghostscript gsfonts-x11 netpbm libpaper-utils libpathplan4 ntp oracle-java8-installer oracle-java8-set-default unzip xfonts-base xorg-sgml-doctools discover htop libcroco3 liblzo2-2 libudev1 libpthread-stubs0-dev --no-install-recommends
 wget https://openresty.org/download/openresty-1.9.7.5.tar.gz
 tar -xvzf openresty-1.9.7.5.tar.gz
 cd openresty-1.9.7.5
 ./configure --with-luajit --with-http_gzip_static_module --with-http_iconv_module --with-ipv6 && make && make install
 cd ..
+wget http://download.gna.org/wkhtmltopdf/0.12/0.12.3/wkhtmltox-0.12.3_linux-generic-amd64.tar.xz
+tar xf wkhtmltox-0.12.3_linux-generic-amd64.tar.xz
+rm wkhtmltox-0.12.3_linux-generic-amd64.tar.xz
+mv wkhtmltox /usr/share
+ln -sf /usr/share/wkhtmltox/bin/wkhtmltopdf /usr/bin/wkhtmltopdf
+ln -sf /usr/share/wkhtmltox/bin/wkhtmltoimage /usr/bin/wkhtmltoimage
 wget https://htstatic.blob.core.windows.net/static/mesos/phantomjs-2.1.1-linux-x86_64.tar.bz2
 export PHANTOM_JS="phantomjs-2.1.1-linux-x86_64"
 tar xvjf $PHANTOM_JS.tar.bz2
